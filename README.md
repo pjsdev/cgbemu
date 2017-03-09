cd3c6fd7d29ab2917f383b2956543a9c80ebc824
Peter Smith <pete@pj-smith.co.uk>
2017-03-09 20:48:51 +0000

    rejigged gcc build on osx

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ The only input to this project is SDL.
 Build
 ======
 
-To build, add the following:
+To build on windows, add the following:
 
     ./run_tree/SDL2.dll
     ./run_tree/obj/
@@ -12,3 +12,7 @@ To build, add the following:
     ./input/include/
     ./input/SDL2.lib
 
+To build on osx, add the following:
+
+    ./run_tree/SDL2.framework/
+    ./run_tree/data/
--- build_mac.sh
+++ build_mac.sh
@@ -1,1 +1,1 @@
-gcc *.c -o main.o -Ilib/SDL2.framework/Headers -Flib  -framework SDL2 && install_name_tool -change @rpath/SDL2.framework/Versions/A/SDL2 @executable_path/lib/SDL2.framework/Versions/A/SDL2 main.o
+gcc src/*.c -o run_tree/cgbemu -Iinclude -Irun_tree/SDL2.framework/Headers -Frun_tree -framework SDL2 && install_name_tool -change @rpath/SDL2.framework/Versions/A/SDL2 @executable_path/SDL2.framework/Versions/A/SDL2 run_tree/cgbemu
