2a071b9c77001384eab80d16949e1b7c807f822e
Pete Smith <pete@ludogear.com>
2017-03-11 09:13:11 +0000

    update build bat with guard

--- build_win.bat
+++ build_win.bat
@@ -1,4 +1,2 @@
-if not defined DevEnvDir (
-    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
-)
+if not defined VSINSTALLDIR call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 call cl.exe src\*.c /Forun_tree\obj\ /Ferun_tree\cgbemu.exe /Iinclude\ /Iinput\include\ /link input\SDL2.lib
